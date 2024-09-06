399494d57cab682f459d461007d02d7f4d073886
FreneyStudios <marpanseri@gmail.com>
2024-09-06 13:47:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import gc
 import importlib
-
+import json
 
 
 SPEED : int
@@ -25,6 +25,19 @@ def SysExit(motive):
 def CSysExit(condition:bool,motive:str):
     if condition:
         sys.exit(motive)
+
+def GetSettings()->str:
+    file_path = 'settings.json'
+
+    #    Step 1: Open the file and load it into a Python dictionary
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Step 2: Convert the dictionary back to a string
+    json_string = json.dumps(data, indent=4)
+
+    # Step 3: Print or use the string
+    print(json_string)
     
 def Modular(modulename: str, modulepack: str):
     # Check if the module is already imported
